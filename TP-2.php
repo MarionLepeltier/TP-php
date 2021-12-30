3d5a182c1ecf0df0c1324e6f1f15af6e812192c2
unknown <lepeltiermarion@hotmail.fr>
2021-12-30 16:50:15 +0100

    📦 NEW: initial commit

---
+++ TP-2.php
@@ -0,0 +1,18 @@
+<?php
+
+    // Creer une fonction pour verifier si nous avons un palindrome
+
+    $string1 = "kayak";
+    $stringverif ="kayak";
+
+    function palindrome($string1){
+        if ($string1 == $stringverif){
+        echo  " $string1 est un palindrome";
+    } else {
+        echo  "$string ce n'est pas un palindrome";
+    }
+
+    // Creer une fonction pour verifier si c'est un anagrame
+
+
+?>
